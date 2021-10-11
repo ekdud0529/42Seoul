b58b37e2d5991d73c478785d8915d000140ff855
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-10-11 19:56:45 +0900

    Add files via upload

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 16:41:09 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/11 17:06:45 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int	nb)
+{
+	char	number[11];
+	int		cnt;
+	int		num;
+
+	cnt = 0;
+	num = nb;
+	if (nb == 0)
+		number[cnt++] = '0';
+	if (nb < 0)
+		nb *= -1;
+	while (nb != 0)
+	{
+		number[cnt] = nb % 10 + '0';
+		nb /= 10;
+		cnt++;
+	}
+	if (num < 0)
+		number[cnt++] = '-';
+	while (cnt--)
+	{
+		write(1, &number[cnt], 1);
+	}
+}
