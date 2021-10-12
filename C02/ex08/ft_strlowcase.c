cee260b81c3af7da3d4bf09dffb3e13165d154d9
Dayoung Kim <daykim@c5r5s2.42seoul.kr>
2021-10-12 16:48:07 +0900

    create

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/12 16:40:11 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/12 16:47:50 by daykim           ###   ########.fr       */
+/*   Created: 2021/10/12 16:45:10 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/12 16:47:17 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strupcase(char *str)
+char	*ft_strlowcase(char	*str)
 {
 	while (*str)
 	{
-		if (97 <= *str || *str < 122)
-			*str = *str - 32;
+		if (65 <= *str || *str <= 90)
+			*str = *str + 32;
 		str++;
 	}
 	return (str);
