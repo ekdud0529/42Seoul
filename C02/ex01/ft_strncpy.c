636abd9d48a130f8b5f7802bedb51260b91d7037
Dayoung Kim <daykim@c5r5s2.42seoul.kr>
2021-10-12 16:00:19 +0900

    create

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,36 +6,21 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 15:46:21 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/12 15:49:41 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/12 15:59:14 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	char	*cpy;
 
 	cpy = dest;
-	while(n--)
+	while(*src && n--)
 	{
 		*cpy = *src;
 		cpy++;
 		src++;
 	}
 	*cpy = '\0';
-
 	return dest;
-}
-
-int main(){
-	char dest[10];
-	char src[10] = "home!!";
-
-	ft_strncpy(&dest[0], &src[0], 5);
-
-	int i;
-	for(i=0; i<10; i++)
-		printf("%c", dest[i]);
-
-	return 0;
 }
