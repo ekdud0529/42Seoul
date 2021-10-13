6cb7d31698cc59d026708599493f6d7cdcb57151
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-10-13 20:37:53 +0900

    Update r_dwssap.sh

--- Shell_01_check/ex07/r_dwssap.sh
+++ Shell_01_check/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | awk -F: '{print $1}' | rev | sort -r | grep -v '#' | awk -v l1=$FT_LINE1 -v l2=$FT_LINE2 "NR >= l1 && NR <= l2" | tr '\n' ',' | sed 's/.$/./g' | sed 's/,/, /g' | tr -d "\n"
+cat /etc/passwd | awk -F: '{print $1}' | awk NR%2==0 | grep -v '^#' | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ' ' | sed 's/ /, /g' | sed 's/..$/./g' | tr -d '\n'
