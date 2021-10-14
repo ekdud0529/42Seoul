61488c7a0817b909eb47a5cc29671234cdcc9be4
Dayoung Kim <daykim@c5r5s2.42seoul.kr>
2021-10-14 15:59:03 +0900

    modify

--- C02/ex06/ft_str_is_printable.c
+++ C02/ex06/ft_str_is_printable.c
@@ -6,7 +6,7 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 16:36:17 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/12 16:39:26 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/14 15:58:55 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_str_is_printable(char *str)
 {
 	while (*str)
 	{
-		if (0 <= *str || *str <= 32)
+		if (0 <= *str || *str <= 32 || *str == 127)
 			return (0);
 		str++;
 	}
