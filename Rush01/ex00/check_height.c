2201f6bbfc647380525dbbaf6e875d4f0c9bb281
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-10-17 15:55:57 +0900

    Update check_height.c

--- Rush01/ex00/check_height.c
+++ Rush01/ex00/check_height.c
@@ -1,3 +1,9 @@
+int g_max_h;
+int g_ind_row;
+int g_ind_col;
+int g_cnt;
+int g_check;
+
 int check_rowup_height(int  table[4][4], int    height[16])
 {
     g_check = 1;
@@ -21,4 +27,33 @@ int check_rowup_height(int  table[4][4], int    height[16])
       g_ind_col++;
     }
     return (g_check);
+}
+
+int check_rowdown_height(int    table[4][4], int    height[16])
+{
+    int ind_h;
+
+    ind_h = 4;
+    g_check = 1;
+    g_ind_col = 0;
+    while(g_ind_col < 4 && g_check)
+    {
+        g_ind_row = 3;
+        g_max_h = 0;
+        g_cnt = 0;
+        while(g_ind_row >= 0 && g_check)
+        {
+            if (g_max_h < table[g_ind_row][g_ind_col])
+            {
+                g_max_h = table[g_ind_row][g_ind_col];
+                g_cnt++;
+            }
+g_ind_row--;
+}
+if (height[ind_h] != g_cnt)
+g_check = 0;
+g_ind_col++;
+ind_h++;
+}
+return (g_check);
 }
