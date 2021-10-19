e461091cdefb503a2123f839e578284ed08a8e2a
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-19 11:19:03 +0900

    modify

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,36 +6,18 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 14:34:03 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/16 14:40:46 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/19 11:18:40 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char	*s1, char	*s2)
 {
-	int	result;
-
 	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
-		{
-			if (*s1 > *s2)
-			{
-				result = 1;
-				break;
-			}
-			else
-			{
-				result = -1;
-				break;
-			}
-		}
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	if (!(*s1) && !(*s2))
-		result = 0;
-	else if (!(*s1))
-		result = 1;
-	else
-		result = -1;
-	return (result);
+	return (*s1 - *s2);
 }
-		
