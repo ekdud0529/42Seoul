d2457fbbf574a9b92ee8dac7dc46dd5f5d968ffc
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-21 13:50:32 +0900

    create

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 13:10:44 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/21 13:27:17 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int	index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
