8e9d2e60acdedec58a57b0eb33c4cc5426a76d09
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-26 17:31:31 +0900

    modify

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 16:02:21 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/26 17:31:22 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+extern int	ft_atoi_base(char *str, char *base);
+extern int	check_valid(char *base);
+int			get_len(char *str);
+char		*mk_arr(char *arr, int size, int sign);
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int			idx;
+	long long 	num;
+	char		arr[33];
+	int			base_to_len;
+	int			sign;
+
+	sign = 1;
+	base_to_len = get_len(base_to);
+	if (!get_len(base_from) || !base_to_len 
+		|| !check_valid(base_from) || !check_valid(base_to))
+		return (0);
+	num = ft_atoi_base(nbr, base_from);
+	idx = 0;
+	if (num == 0)
+		arr[idx++] = num;
+	if (num < 0)
+	{
+		sign = -1;
+		num *= -1;
+	}
+	while (num)
+	{
+		arr[idx++] = base_to[num % base_to_len];
+		num /= base_to_len;
+	}
+	return (mk_arr(arr, idx, sign));
+}
+
+char	*mk_arr(char *arr, int size, int sign)
+{
+	int		idx;
+	char	*arry;
+
+	idx = 0;
+	arry = (char *)malloc(sizeof(char) * (size + 1));
+	if (sign == -1)
+		arry[idx++] = '-';
+	while (size--)
+	{
+		arry[idx++] = arr[size];
+	}
+	arry[idx] = '\0';
+	return (arry);	
+}
+
+int	get_len(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 16:08:41 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/26 17:05:44 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+long long	get_power(int base, int power)
+{
+	long long	pow;
+
+	pow = 1;
+	while (power--)
+	{
+		pow *= base;
+	}
+	return (pow);
+}
+
+int	check_valid(char *base)
+{
+	char	*check;
+
+	while (*base)
+	{
+		check = base + 1;
+		if (*base == '+' || *base == '-' || *base == ' '
+			|| (9 <= *base && *base <= 13))
+			return (0);
+		while (*check)
+		{
+			if (*base == *check)
+				return (0);
+			check++;
+		}
+		base++;
+	}
+	return (1);
+}
+
+int	check_num(char str, char *base)
+{
+	while (*base)
+	{
+		if (str == *base)
+			return (1);
+		base++;
+	}
+	return (0);
+}
+
+long long	mk_num(int base_len, char *str, char *base)
+{
+	long long	num;
+	int			index;
+	int			str_len;
+
+	str_len = 0;
+	while (str[str_len] && check_num(str[str_len], base))
+		str_len++;
+	num = 0;
+	while (*str)
+	{
+		index = 0;
+		if (!check_num(*str, base))
+			return (num);
+		while (base[index])
+		{
+			if (base[index] == *str)
+			{
+				num += (index * get_power(base_len, --str_len));
+				break ;
+			}
+			index++;
+		}
+		str++;
+	}
+	return (num);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int			base_len;
+	int			sign;
+	long long	num;
+
+	base_len = 0;
+	while (base[base_len])
+		base_len++;
+	if (base_len < 2 || !check_valid(base) || str[0] == '\0')
+		return (0);
+	sign = 1;
+	while ((9 <= *str && *str <= 13) || *str == ' ')
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	num = mk_num(base_len, str, base);
+	num *= sign;
+	if (num > 2147483647 || num < -2147483648)
+		return (0);
+	return ((int)num);
+}
