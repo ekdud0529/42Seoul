eee135eb6b298c334876bc4c1890daa1c9bb7f71
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-26 23:01:34 +0900

    modify

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:48:09 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/26 22:46:47 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/26 23:01:24 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,58 @@ int	ch_charset(char c, char *charset);
 char	**ft_split(char *str, char *charset)
 {
 	char	**arr;
-	char	*strr;
 	int		word_cnt;
 	int		idx;
 	int		i;
+	int		len;
 
 	word_cnt = 0;
+	cnt_word(str, charset, &word_cnt);
+	arr = (char **) malloc (sizeof(char *) * (word_cnt + 1));
+	i = 0;
+	idx = 0;
 	while (str[i])
 	{
 		while (ch_charset(str[i], charset))
 			i++;
+		len = i;
 		while (!ch_charset(str[i], charset))
 			i++;
-		word_cnt++;
+		if (len > i)
+		{
+			arr[idx] = mk_str(len, i, str);
+			idx++;
+		}
 	}
-	arr = (char **) malloc (sizeof(char *) * word_cnt);
+	return (arr);
+}
 
+void	mk_str(int	start, int end, char *str)
+{
+	char	*arr;
+	int		i;
+
+	i = 0;
+	arr = (char *) malloc (sizeof(char) * (end - start +1));
+	while (start < end)
+	{
+		arr[i] = str[start];
+		start++;
+		i++;
+	}
+	return (arr);
+}
+
+void	cnt_word(char *str, char *charset, int *cnt)
+{
+	while (str[i])
+	{
+		while (ch_charset(str[i], sharset))
+			i++;
+		while (!ch_charset(str[i], charset))
+			i++;
+		*cnt++;
+	}
 }
 
 int	ch_charset(char c, char *charset)
