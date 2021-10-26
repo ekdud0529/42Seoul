79f0cfd568a9fbfc8880617ba6393bbe9c5a12ca
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-27 00:42:11 +0900

    create

---
+++ C08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 23:52:07 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/27 00:07:54 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+extern void	ft_putchar(char c);
+extern void	ft_swap(int *a, int *b);
+extern void	ft_putstr(char *str);
+extern int	ft_strlen(char *str);
+extern int	ft_strcmp(char *s1, char *s2);
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 00:20:25 by daykim            #+#    #+#             */
+/*   Updated: 2021/10/27 00:41:44 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	str_len(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*str_cpy(char *str)
+{
+	int		i;
+	char	*arr;
+
+	i = 0;
+	while (str[i])
+		i++;
+	arr = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i])
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	return (arr);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*arr;
+
+	arr = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!arr)
+		return (0);
+	while (i < ac)
+	{
+		arr[i].size = str_len(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = str_cpy(av[i]);
+	}
+	return (arr);
+}
