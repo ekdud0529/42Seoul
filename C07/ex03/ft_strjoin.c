7e2e6203d265f9c1957cfd5f7bcb41e94bd7124a
Dayoung Kim <daykim@c5r5s5.42seoul.kr>
2021-10-26 16:01:43 +0900

    modify

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,14 +6,14 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 13:27:04 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/26 13:59:25 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/26 16:01:19 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 char	*ft_strcat(char *dest, char *src);
-int	get_len(char *str);
+int		get_len(char *str);
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
@@ -23,25 +23,28 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 
 	i = -1;
 	len = 0;
-	len += get_len(sep) * (size - 1);
-	while (++i < size)
-		len += get_len(strs[i]);
-	str = (char *) malloc (sizeof(char) * (len + 1));
-	if (size == 0)
+	if (size <= 0)
 	{
+		str = (char *)malloc(sizeof(char) * 1);
 		str[0] = '\0';
 		return (str);
 	}
-	i = 0;
-	while (i < size)
+	len += (get_len(sep) * (size - 1));
+	while (++i < size)
+		len += get_len(strs[i]);
+	str = (char *) malloc (sizeof(char) * (len + 1));
+	i = -1;
+	str[0] = '\0';
+	while (++i < size)
 	{
-		ft_strcat(str, strs[i++]);
-		ft_strcat(str, sep);
+		ft_strcat(str, strs[i]);
+		if (i != size - 1)
+			ft_strcat(str, sep);
 	}
-	str[i] = '\0';
+	return (str);
 }
 
-int	 get_len(char *str)
+int	get_len(char *str)
 {
 	int	len;
 
