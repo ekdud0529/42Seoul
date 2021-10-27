d27dfb5ec0c4ecd1cea8963afc19f96e952cd4f5
Dayoung Kim <daykim@c3r7s1.42seoul.kr>
2021-10-27 11:07:12 +0900

    modify

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 16:02:21 by daykim            #+#    #+#             */
-/*   Updated: 2021/10/26 21:35:14 by daykim           ###   ########.fr       */
+/*   Updated: 2021/10/27 11:07:00 by daykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ extern int	ft_atoi_base(char *str, char *base);
 extern int	check_valid(char *base);
 int			get_len(char *str);
 char		*mk_arr(char *arr, int size, int sign);
+int			ch_base(char *base_from, char *base_to);
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
@@ -27,8 +28,7 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 
 	sign = 1;
 	base_to_len = get_len(base_to);
-	if (!get_len(base_from) || !base_to_len
-		|| !check_valid(base_from) || !check_valid(base_to))
+	if (!ch_base(base_from, base_to))
 		return (0);
 	num = ft_atoi_base(nbr, base_from);
 	idx = 0;
@@ -45,6 +45,14 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 		num /= base_to_len;
 	}
 	return (mk_arr(arr, idx, sign));
+}
+
+int	ch_base(char *base_from, char *base_to)
+{
+	if (!get_len(base_to) || !get_len(base_from)
+		|| !check_valid(base_from) || !check_valid(base_to))
+		return (0);
+	return (1);
 }
 
 char	*mk_arr(char *arr, int size, int sign)
